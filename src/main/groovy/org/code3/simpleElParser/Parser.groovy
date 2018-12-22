b27a597d7238acb685502a2cf5ef372a612d225e
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-12-22 04:58:54 -0500

    make it testable

---
+++ src/main/groovy/org/code3/simpleElParser/Parser.groovy
@@ -0,0 +1,7 @@
+package org.code3.simpleELParser
+
+public class Parser {
+  static List parse(String expression){
+    return []
+  }
+}
---
+++ src/test/groovy/ParserTest.groovy
@@ -0,0 +1,45 @@
+import org.code3.simpleELParser.Parser
+import groovy.json.JsonSlurper
+import groovy.util.GroovyTestCase
+
+class ParserTest  extends GroovyTestCase {
+  def getFile(pathInClassPath){
+    println pathInClassPath
+    new File(getClass().getResource(pathInClassPath).file)
+  }
+  void testAll(){
+    def tests = [
+       "001-and",
+       "002-or",
+       "003-not",
+       "004-eq",
+       "005-lt",
+       "006-gt",
+       "007-lte",
+       "008-gte",
+       "009-neq",
+       "011-string-literral",
+       "012-integer-literral",
+       "013-float-literral",
+    ]
+    .each {
+      println "Testing $it"
+      def unit = [
+        test: it,
+        // error: getFile("/simple-el-parser-test-suite/$it/error.txt").text.trim(),
+        exp: getFile("/simple-el-parser-test-suite/$it/exp.txt").text.trim(),
+        result: new JsonSlurper().parseText(getFile("/simple-el-parser-test-suite/$it/result.json").text.trim())
+      ]
+
+      try {
+        def parsedExpression = Parser.parse(unit.exp)
+        def expected = unit.result
+        assert  expected == parsedExpression
+      } catch (Exception e){
+        def expectedError = unit.error
+        def actualError = e.message
+        assert expectedError ==  actualError
+      }
+    }
+  }
+}
